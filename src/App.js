1408abca36dcdc71f4e9ef54a73461e498d08fbb
Christian Bound <automaticbound@gmail.com>
2022-09-26 14:36:03 +0900

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Component } from 'react'
 import './App.scss';
+import SearchBar from './components/searchBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className='left-scene'></div>
+        <div className='left-scene'>
+          <SearchBar />
+          <div className='selected-gif'></div>
+        </div>
         <div className='right-scene'></div>
       </div>
     )
---
+++ src/components/searchBar/index.js
@@ -0,0 +1,8 @@
+
+const SearchBar = () => {
+  return (
+    <input type="text" className="form-control form-search" />
+  );
+}
+
+export default SearchBar;
